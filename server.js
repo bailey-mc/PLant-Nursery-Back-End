f3f7c9e8e64bc267ce05438d28d9ebd053f99886
Bailey McDonald <mcdonald.bailey@gmail.com>
2022-12-09 15:50:07 -0600

    made get route for '/' for heroku

--- server.js
+++ server.js
@@ -21,40 +21,40 @@ app.get('/', (req, res) => {
   
 })
 
-// // create route
-// app.post('/plantnursery', (req, res) => {
-//   Plant.create(req.body, (err, createdPlant) => {
-//     res.json(createdPlant)
-//   })
-// })
+// create route
+app.post('/plantnursery', (req, res) => {
+  Plant.create(req.body, (err, createdPlant) => {
+    res.json(createdPlant)
+  })
+})
 
-// // read route
-// app.get('/plantnursery', (req, res) => {
-//   Plant.find({}, (err, foundPlants) => {
-//     res.json(foundPlants)
-//   })
-// })
+// read route
+app.get('/plantnursery', (req, res) => {
+  Plant.find({}, (err, foundPlants) => {
+    res.json(foundPlants)
+  })
+})
 
-// // update route
-// app.put('/plantnursery/:id', (req, res) => {
-//   Plant.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedPlant) => {
-//     res.json(updatedPlant);
-//   })
-// })
+// update route
+app.put('/plantnursery/:id', (req, res) => {
+  Plant.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedPlant) => {
+    res.json(updatedPlant);
+  })
+})
 
-// // delete route
-// app.delete('/plantnursery/:id', (req, res) => {
-//   console.log(req.params)
-//   Plant.findByIdAndDelete(req.params.id, (err, deletedPlant) => {
-//     res.json(deletedPlant);
-//   })
-// })
+// delete route
+app.delete('/plantnursery/:id', (req, res) => {
+  console.log(req.params)
+  Plant.findByIdAndDelete(req.params.id, (err, deletedPlant) => {
+    res.json(deletedPlant);
+  })
+})
 
-// // LISTENER
-// mongoose.connect('mongodb://localhost:27017/plantnursery-MERN')
-// mongoose.connection.once('open', () => {
-//   console.log('connected to mongosh...')
-// });
+// LISTENER
+mongoose.connect('mongodb://localhost:27017/plantnursery-MERN')
+mongoose.connection.once('open', () => {
+  console.log('connected to mongosh...')
+});
 
 // PORT
 app.listen(PORT, () => {
