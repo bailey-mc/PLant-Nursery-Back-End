da795f51458c9a0304dd33e0ee0a7f804b3a479b
Bailey McDonald <mcdonald.bailey@gmail.com>
2022-12-09 15:42:32 -0600

    tests for heoku

--- server.js
+++ server.js
@@ -16,45 +16,45 @@ app.use(cors());
 
 //route for heroku
 app.get('/', (req, res) => {
-  Plant.find({}, (err, foundPlants) => {
-    res.json(foundPlants)
-  })
+  console.log('hiii');
+  res.send('hi')
+  
 })
 
-// create route
-app.post('/plantnursery', (req, res) => {
-  Plant.create(req.body, (err, createdPlant) => {
-    res.json(createdPlant)
-  })
-})
+// // create route
+// app.post('/plantnursery', (req, res) => {
+//   Plant.create(req.body, (err, createdPlant) => {
+//     res.json(createdPlant)
+//   })
+// })
 
-// read route
-app.get('/plantnursery', (req, res) => {
-  Plant.find({}, (err, foundPlants) => {
-    res.json(foundPlants)
-  })
-})
+// // read route
+// app.get('/plantnursery', (req, res) => {
+//   Plant.find({}, (err, foundPlants) => {
+//     res.json(foundPlants)
+//   })
+// })
 
-// update route
-app.put('/plantnursery/:id', (req, res) => {
-  Plant.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedPlant) => {
-    res.json(updatedPlant);
-  })
-})
+// // update route
+// app.put('/plantnursery/:id', (req, res) => {
+//   Plant.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedPlant) => {
+//     res.json(updatedPlant);
+//   })
+// })
 
-// delete route
-app.delete('/plantnursery/:id', (req, res) => {
-  console.log(req.params)
-  Plant.findByIdAndDelete(req.params.id, (err, deletedPlant) => {
-    res.json(deletedPlant);
-  })
-})
+// // delete route
+// app.delete('/plantnursery/:id', (req, res) => {
+//   console.log(req.params)
+//   Plant.findByIdAndDelete(req.params.id, (err, deletedPlant) => {
+//     res.json(deletedPlant);
+//   })
+// })
 
-// LISTENER
-mongoose.connect('mongodb://localhost:27017/plantnursery-MERN')
-mongoose.connection.once('open', () => {
-  console.log('connected to mongosh...')
-});
+// // LISTENER
+// mongoose.connect('mongodb://localhost:27017/plantnursery-MERN')
+// mongoose.connection.once('open', () => {
+//   console.log('connected to mongosh...')
+// });
 
 // PORT
 app.listen(PORT, () => {
