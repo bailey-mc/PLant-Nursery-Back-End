88bb6a298f333ee4c4304001e6245fcb100c387c
Bailey McDonald <mcdonald.bailey@gmail.com>
2022-12-12 10:27:27 -0600

    cors config

--- server.js
+++ server.js
@@ -10,7 +10,28 @@ require('dotenv').config()
 // if(process.env.PORT){
 // 	PORT = process.env.PORT
 // }
-let PORT = process.env.PORT;
+let PORT = process.env.PORT || 3000;
+
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+
+const domainsFromEnv = process.env.CORS_DOMAINS || ""
+
+const whitelist = domainsFromEnv.split(",").map(item => item.trim())
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 
 
