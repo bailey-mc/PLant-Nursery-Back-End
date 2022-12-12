a5d71f201f9454a319c59ddd71d0f044231ef143
Bailey McDonald <mcdonald.bailey@gmail.com>
2022-12-12 10:31:43 -0600

    cors config

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config()
 // const Plant = require('./models/plantSchema.js')
+const app = express();
 
 
 //port stuff for heroku
@@ -29,9 +30,8 @@ app.use(cors(corsOptions))
 
 
 //middleware
-const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 //////////////////////////
 //CONTROLLERS
