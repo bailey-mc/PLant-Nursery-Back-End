c546111dc4ce1a4c8917029a0ab0f6c7facb5371
Bailey McDonald <mcdonald.bailey@gmail.com>
2022-12-13 10:43:53 -0600

    backend built out for user suthentication

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const cors = require('cors');
 require('dotenv').config()
 // const Plant = require('./models/plantSchema.js')
@@ -16,6 +17,8 @@ let PORT = process.env.PORT || 3001;
 //middleware
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
 app.use(cors());
 
 //////////////////////////
@@ -23,6 +26,8 @@ app.use(cors());
 ////////////////////////
 const plantController = require('./controllers/plantroutes')
 app.use('/plantnursery', plantController)
+const usersController = require('./controllers/users')
+app.use('/users', usersController)
 
 ///////////////////////////
 //ROUTES
@@ -36,55 +41,26 @@ app.use((req,res, next)=> {
   res.send('404 page not found')
 })
 
-// seed route
-const plantSeed = require('./models/plantData.js')
+// // seed route
+// const plantSeed = require('./models/plantData.js')
 
-app.get('/seed', (req, res) => {
-  Plant.create(plantSeed, (error, data) => {
-    res.send('database seeded with: ' + data)
-  })
-})
-
-//route for heroku
-// app.get('/', (req, res) => {
-//   console.log('hiii');
-//   res.send('hi')
-// })
-
-// // create route
-// app.post('/plantnursery/new', (req, res) => {
-//   Plant.create(req.body, (err, createdPlant) => {
-//     res.json(createdPlant)
-    
+// app.get('/seed', (req, res) => {
+//   Plant.create(plantSeed, (error, data) => {
+//     res.send('database seeded with: ' + data)
 //   })
 // })
 
-// // read route
-// app.get('/plantnursery', (req, res) => {
-//   Plant.find({}, (err, foundPlants) => {
-//     console.log(foundPlants);
-//     // res.send('/plantnursery')
-//     res.json(foundPlants)
-//   })
-// })
 
-// // update route
-// app.put('/plantnursery/:id', (req, res) => {
-//   Plant.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedPlant) => {
-//     res.json(updatedPlant);
-//   })
-// })
-
-// // delete route
-// app.delete('/plantnursery/:id', (req, res) => {
-//   console.log(req.params)
-//   Plant.findByIdAndDelete(req.params.id, (err, deletedPlant) => {
-//     res.json(deletedPlant);
-//   })
-// })
 
 // LISTENER
-mongoose.connect('mongodb+srv://musiclover:bobdylaN%2196@mymusic.wef1hdo.mongodb.net/?retryWrites=true&w=majority', ()=>{
+mongoose.connect('mongodb+srv://emonga:aBB4E9oTaQ48nM&Y@cluster0.0eubsxh.mongodb.net/?retryWrites=true&w=majority', 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+},
+()=>{
 	console.log('connected to mongo');
 })
 
